2887f408643acff9bc1a66f4dfb13a92e9adfb7a
wesley_hataway <wesleyjh@uab.edu>
2021-10-04 14:25:57 -0500

    updated primeNumbers.py

--- fastEnterprises/primeNumbers.py
+++ fastEnterprises/primeNumbers.py
@@ -1,3 +1,18 @@
 
 def primeNumberChecker(x):
-    return 
+    if x > 1:
+        for i in range(2, int(x/2)+1):
+            if x%i == 0:
+                return False
+        return True
+    return False
+
+def makePrimeList(x):
+    primeNumberList = []
+    for i in range(x):
+        if primeNumberChecker(i):
+            primeNumberList.append(i)
+    return primeNumberList
+
+print(primeNumberChecker(21), 21)
+print(makePrimeList(100))
