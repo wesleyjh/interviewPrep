09d8e24031b68e96f7d1c98e5977eafe4d407046
wesley_hataway <wesleyjh@uab.edu>
2021-11-15 14:29:29 -0600

    Added First coding question in the final round interview.

---
+++ fastEnterprises/codingProblemOne.py
@@ -0,0 +1,13 @@
+# Write a function that takes in a string, s, and returns the amount of 'a' in the string.
+# There is a second input, x, that tells you how many characters to go up to. 
+# If x is greater than the length of s, go back through s up until you reach x characters.
+
+# I.e 
+# s = "abca", x = 3
+# The correct answer here is 1.
+# s = "abca", x = 9
+# The correct answer is 5. s in this instance s "abcaabcaa"
+
+
+def aCounter(s, x):
+    pass
